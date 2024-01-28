5b1e4db0e4f8eb9dd4b59827e278471964ed74b7
Kies0 <157509070+Kies0@users.noreply.github.com>
2024-01-28 08:25:45 +0100

    Fixed the decoded output
    Adding a loop for encoding and decoding more

--- main.py
+++ main.py
@@ -8,16 +8,40 @@ shift = int(input("Type the shift number:\n"))
 def caesar(start_text, shift_amount, cipher_direction):
 	word_encoded = True
 	cipher_text = " "
+	if cipher_direction == "decode":
+		shift_amount *= -1 
+		word_encoded = False
 	for i in start_text:
-		if cipher_direction == "decode":
-			shift_amount = shift_amount * -1
-			word_encoded = False
-		cipher_text += alphabet[(alphabet.index(i) + shift) % 26]			
+		if i in alphabet:
+			cipher_text += alphabet[(alphabet.index(i) + shift_amount) % 26]
+		else:
+			cipher_text += i
+		
+		
 
-
-	if (word_encoded):
+	if word_encoded:
 		print(f"The encoded text is {cipher_text}")
 	else:
 		print(f"The decoded text is {cipher_text}")
 
 caesar(text, shift, direction)
+
+
+again = True
+play_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+
+while again:
+	direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+	text = input("Type your message:\n").lower()
+
+	shift = int(input("Type the shift number:\n"))
+	caesar(text, shift, direction)
+	play_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+
+	if play_again == "no":
+		again = False
+	elif play_again == "yes":
+		again = True
+	else:
+		print("Invalid input.")
+		again = False
